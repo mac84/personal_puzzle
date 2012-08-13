fd498c83a9b5fcceda19d1b68a82785c2ba935e8
Johan Wallén <hulqen@gmail.com>
2012-08-13 16:01:14 +0200

    Add tests for user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,12 @@ class User < ActiveRecord::Base
   has_many :tasks, :dependent => :destroy
   has_many :clients, :through => :tasks
 
-  before_save :encrypt_password
+  before_save :encrypt_password, :if => proc { password.present? }
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = BCrypt::Engine.generate_salt
-      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-    end
-  end
 
   def self.authenticate(email, password)
     user = find_by_email(email)
@@ -28,4 +21,10 @@ class User < ActiveRecord::Base
     end
   end
 
+  protected
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -9,4 +9,40 @@ describe "User" do
     end
   end
 
+  describe "saving a user" do
+    it "generates password salt and hash if password is set" do
+      user = User.make(:password => "1234")
+      user.password_hash.should eq(nil)
+      user.save
+
+      user.password_hash.should eq(BCrypt::Engine.hash_secret(user.password, user.password_salt))
+    end
+
+    it "does not generate a password salt and hash if password is not set" do
+      user = User.make(:password => nil)
+      expect { user.save }.to_not change { user.password_hash }
+    end
+  end
+
+  describe ".authenticate" do
+    it "returns nothing if no user is found" do
+      email = "hej@hej.se"
+      password = "1234"
+      User.authenticate(email, password).should eq(nil)
+    end
+
+    it "returns user if user with the given password is found" do
+      email = "hej@hej.se"
+      password = "1234"
+      user = User.make!(:email => email, :password => password)
+      User.authenticate(email, password).should eq(user)
+    end
+
+    it "returns nothing if wrong password is given" do
+      email = "hej@hej.se"
+      password = "1234"
+      user = User.make!(:email => email, :password => password)
+      User.authenticate(email, "wrong_password").should eq(nil)
+    end
+  end
 end
