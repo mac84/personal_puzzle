6752ebc773f45ca1f44b5f7f338159b5d19c97a0
Johan Wallén <hulqen@gmail.com>
2012-08-14 12:52:33 +0200

    Add basic blueprints for task and client

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -5,3 +5,12 @@ User.blueprint do
   email           { "User#{sn}@foo.com" }
   password        { "password #{sn}" }
 end
+
+Task.blueprint do
+  name            { "name#{sn}" }
+end
+
+Client.blueprint do
+  name            { "name#{sn}" }
+end
+
