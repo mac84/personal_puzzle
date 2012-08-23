053ed5dfa44d3cb8fc42003f7aeed2d49192c8f1
Johan Wallén <hulqen@gmail.com>
2012-08-23 10:29:56 +0200

    Add controller methods to show and update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,32 @@
 # encoding: utf-8
 class UsersController < ApplicationController
   skip_before_filter :require_login, :only => [:new, :create]
+
   def new
     @user = User.new
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to user_path(@user), :notice => "Användaren uppdaterad!"
+    else
+      render "edit"
+    end
   end
 
   def create
     @user = User.new(params[:user])
 
     if @user.save
-      session[:user_id] = user.id
       cookies[:auth_token] = user.auth_token
       redirect_to root_url, :notice => "Användare skapad! Välkommen!"
     else
