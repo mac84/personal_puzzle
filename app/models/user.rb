6b4e53cf4ce96ea0f72566e18c124cd7a2a75ccd
Johan Wallén <hulqen@gmail.com>
2012-08-31 09:34:13 +0200

    Add schedule feature (continued)
    The model with the schedule feature itself

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@
 class User < ActiveRecord::Base
   Password = BCrypt::Password
 
-  attr_accessible :email, :password, :password_confirmation, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday, :work_shifts_attributes
+  attr_accessible :email, :password, :password_confirmation, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday, :min_scheduled_shift_length, :work_shifts_attributes
 
   has_many :tasks,       :dependent => :destroy
   has_many :clients
   has_many :work_shifts, :dependent => :destroy
   has_many :completed_shifts, :through => :tasks
+  has_many :scheduled_shifts, :through => :tasks
 
   accepts_nested_attributes_for :work_shifts, :allow_destroy => true
 
@@ -49,6 +50,10 @@ class User < ActiveRecord::Base
     begin
       self[column] = SecureRandom.urlsafe_base64
     end while User.exists?(column => self[column])
+  end
+
+  def clear_schedule
+    ScheduledShift.where(:task_id => self.task_ids).delete_all
   end
 
   def daily_worktime
@@ -166,6 +171,148 @@ class User < ActiveRecord::Base
   def number_of_finished_tasks_per_client
     self.tasks.where("date_finished is NOT NULL").each_with_object(Hash.new(0)) do |ft, result|
       result[[ft.client_name]] += 1
+    end
+  end
+
+  def schedule
+    active_tasks = self.tasks.where(:date_finished => nil, :archived => false).order("deadline_date")
+
+    active_tasks.each do |task_to_schedule|
+      task_duration = ((task_to_schedule.fee.to_f / task_to_schedule.hourly_rate).ceil) - completed_shifts_duration(task_to_schedule)
+      today = Time.now
+      puts today
+      # puts today.to_datetime
+      start_date = today.beginning_of_hour + 1.hour
+      puts start_date
+
+      while task_duration > 0
+        if self.works_on(today) && (unscheduled_work_time_left(today + (last_scheduled_shift_end_time(today) * 1.minute)) >= (self.min_scheduled_shift_length * 1.minute))
+
+          start_date += ((last_scheduled_shift_end_time(today) - (start_date.hour * 60)) * 60) if any_scheduled_shifts(today)
+
+          if work_shift_has_started(start_date, closest_work_shift_from(today)) && ((end_time_for(closest_work_shift_from(today)) - start_date.hour) < self.min_scheduled_shift_length)
+            next_work_shift = closest_work_shift_from(start_date + (((end_time_for(closest_work_shift_from(start_date))) * 1.hour) - (start_date.hour * 3600)))
+            start_date += ((next_work_shift.start_time.hour + (next_work_shift.start_time.min.to_f / 60)) - start_date.hour) * 3600
+            puts start_date
+          end
+
+          task_to_schedule.scheduled_shifts.create(:start_date => (start_date), :duration => scheduled_shift_duration(task_duration))
+
+          task_duration -= scheduled_shift_duration(task_duration)
+          today += 1.day
+          today = today.beginning_of_day
+          start_date = today
+          first_work_shift = self.work_shifts.order(:start_time).first
+          start_date += ((first_work_shift.start_time.hour * 3600) + (first_work_shift.start_time.min * 60))
+        else
+          today += 1.day
+          today = today.beginning_of_day
+          start_date = today
+          first_work_shift = self.work_shifts.order(:start_time).first
+          start_date += ((first_work_shift.start_time.hour * 3600) + (first_work_shift.start_time.min * 60))
+        end
+
+      end
+      # puts self.scheduled_shifts.all.inspect
+    end
+
+  end
+
+  def closest_work_shift_from(now)
+    self.work_shifts.order("start_time").detect { |shift| (shift.start_time.hour * 60) + shift.start_time.min + (shift.duration * 60) > ((now.hour * 60) + now.min) }
+  end
+
+  def completed_shifts_duration(task)
+    total_duration = 0
+    task.completed_shifts.each do |cs|
+      total_duration += cs.duration
+    end
+    total_duration
+  end
+
+  def last_scheduled_shift(today)
+    self.scheduled_shifts.where("start_date between ? and ?", today.beginning_of_day, today.end_of_day).order(:start_date).last
+  end
+
+  def last_scheduled_shift_end_time(today) # Kan rationaliseras bort och ersättas med en kombination av last_sched och end_time_for
+    if self.scheduled_shifts.where("start_date between ? and ?", (today.beginning_of_day + today.gmt_offset), (today.end_of_day + today.gmt_offset)).size > 0
+      last_scheduled_shift_today = self.scheduled_shifts.where("start_date between ? and ?", today.beginning_of_day, today.end_of_day).order(:start_date).last
+      last_scheduled_shift_today_end_time = last_scheduled_shift_today.start_date + (last_scheduled_shift_today.duration * 3600)
+      last_scheduled_shift_today_end_time.min + (last_scheduled_shift_today_end_time.hour * 60)
+    else
+      0
+    end
+  end
+
+  def any_scheduled_shifts(today)
+    self.scheduled_shifts.where("start_date between ? and ?", (today.beginning_of_day + today.gmt_offset), (today.end_of_day + today.gmt_offset)).size > 0
+  end
+
+  def unscheduled_work_time_left(today)
+    if any_scheduled_shifts(today)
+      work_time_left(today) - last_scheduled_shift_end_time(today)
+    else
+      work_time_left(today)
+    end
+  end
+
+  def work_time_left(today)
+    last_work_shift_today = self.work_shifts.order("start_time").last
+    end_of_last_work_shift_today = last_work_shift_today.start_time + (last_work_shift_today.duration * 3600)
+
+    end_of_last_work_shift_today_in_hours = ((end_of_last_work_shift_today.min.to_f / 60) + end_of_last_work_shift_today.hour)
+
+    start_of_next_hour = today.beginning_of_hour + 1.hour
+
+    work_shifts_left_today = self.work_shifts.order("start_time").find_all { |shift| (shift.start_time.hour * 60) + shift.start_time.min + (shift.duration * 60) > (start_of_next_hour.hour * 60) }
+    closest_work_shift_from_now = work_shifts_left_today.first
+
+    if start_of_next_hour.hour < end_of_last_work_shift_today_in_hours
+
+      if work_shift_has_started(start_of_next_hour, closest_work_shift_from_now)
+        elapsed_worktime_of_closest_work_shift = closest_work_shift_from_now.duration - (end_time_for(closest_work_shift_from_now) - start_of_next_hour.hour)
+
+        (closest_work_shift_from_now.duration - elapsed_worktime_of_closest_work_shift + remaining_work_shift_duration(work_shifts_left_today)) * 60
+      else
+        (closest_work_shift_from_now.duration + remaining_work_shift_duration(work_shifts_left_today)) * 60
+      end
+    else
+      0
+    end
+  end
+
+  def work_shift_has_started(now, closest_work_shift_from_now)
+    now.hour > closest_work_shift_from_now.start_time.hour && now.hour < end_time_for(closest_work_shift_from_now)
+  end
+
+  def end_time_for(shift)
+    ((shift.start_time.hour * 60) + shift.start_time.min + (shift.duration * 60)).to_f / 60
+  end
+
+  def remaining_work_shift_duration(work_shifts_left_today)
+    remaining_work_shift_duration = 0
+    work_shifts_left_today.each_with_index do |ws, i|
+      remaining_work_shift_duration += ws.duration if i > 0
+    end
+    remaining_work_shift_duration
+  end
+
+  def scheduled_shift_duration(task_duration)
+    if task_duration >= self.min_scheduled_shift_length
+      self.min_scheduled_shift_length
+    else
+      task_duration
+    end
+  end
+
+  def schedule_up_to_date?
+    latest_scheduled_shift = self.scheduled_shifts.order("created_at DESC").first
+    latest_completed_shift = self.completed_shifts.order("created_at DESC").first
+
+    if latest_scheduled_shift && latest_completed_shift
+      latest_completed_shift.created_at < latest_scheduled_shift.created_at
+    else
+      false
     end
   end
 
